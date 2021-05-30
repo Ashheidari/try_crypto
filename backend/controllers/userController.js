b48538e5da76916c9a40d942903d47470b872256
Arash Heidari <arashheidari@Arashs-MacBook-Pro.local>
2021-05-30 16:45:09 +0430

    uimplement usersignup validation & handle errors

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,27 @@
 const HttpError = require('../models/http-error');
 const User = require('../models/User');
+const {validationResult} = require('express-validator')
 
 
 const signupUser = (req, res, next)=>{
-
+    const validationErrors = validationResult(req);
+    if(!validationErrors.isEmpty()){
+        const error = new HttpError(validationErrors.errors[0].msg,422)
+        throw error
+    }
     const name = req.body.name;
     const lastname = req.body.lastname;
     const email = req.body.email;
     const password = req.body.password;
     
 
-    const user =new User({name,lastname,email,password})
-    user.save().then(result=>{
-        console.log('user saved sucssesfuly')
-        res.json(result);
-    }).catch(err=>{
-        next(err);
-    })
+    // const user =new User({name,lastname,email,password})
+    // user.save().then(result=>{
+    //     console.log('user saved sucssesfuly')
+    //     res.json(result);
+    // }).catch(err=>{
+    //     next(err);
+    // })
     
 
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,17 +1,28 @@
-const express = require('express');
+const express = require("express");
+const { check } = require("express-validator");
 
-
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
+router.put(
+  "/signup",
+  [
+    check("name").notEmpty().withMessage('name is required').trim().toLowerCase(),
+    check("lastname").notEmpty().withMessage('lastname is required').trim().toLowerCase(),
+    check("email").normalizeEmail().isEmail().withMessage('Enter a valid email address'),
+    check("password").trim().isLength({min:6}).withMessage('password must at least 6 char'),
+    check("confirmpassword").custom((value,{req})=>{
+        if(value !== req.body.password){
+            throw new Error('passwords need to match')
+        }else{return true}
+    }),
+  ],
+  userController.signupUser
+);
 
-router.post('/signup',userController.signupUser);
+router.post("/login", userController.loginUser);
 
-router.post('/login',userController.loginUser)
+router.post("/pay", userController.payUser);
 
-router.post('/pay',userController.payUser)
-
-
-
-module.exports= router
+module.exports = router;
